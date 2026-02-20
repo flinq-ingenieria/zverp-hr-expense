f3830f1ef486a65b937fd79f58749d6a13c0b4f5
Pablo Castelo <pablocv@gmx.es>
2026-02-20 17:01:15 +0100

    fix

---
+++ hr_expense_auto_validate/models/hr_expense.py
@@ -0,0 +1,31 @@
+from odoo import _, models
+
+
+class HrExpense(models.Model):
+    _inherit = "hr.expense"
+
+    def action_submit_expenses(self):
+        """Create report(s) and trigger auto submit/approve/post flow."""
+        context_vals = self._get_default_expense_sheet_values()
+        sheets = self.env["hr.expense.sheet"].create(context_vals)
+        sheets.sudo().action_submit_sheet()
+
+        if len(sheets) > 1:
+            return {
+                "name": _("Expense Reports"),
+                "type": "ir.actions.act_window",
+                "views": [[False, "list"], [False, "form"]],
+                "res_model": "hr.expense.sheet",
+                "domain": [("id", "in", sheets.ids)],
+                "context": self.env.context,
+            }
+
+        return {
+            "name": _("Expense Report"),
+            "type": "ir.actions.act_window",
+            "views": [[False, "form"]],
+            "res_model": "hr.expense.sheet",
+            "res_id": sheets.id,
+            "target": "current",
+            "context": self.env.context,
+        }
