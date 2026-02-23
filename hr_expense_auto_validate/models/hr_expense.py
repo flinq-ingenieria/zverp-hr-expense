4d32ea95089ea8eb149e5a5ca550b70545894c99
Pablo Castelo <pablocv@gmx.es>
2026-02-23 15:52:25 +0100

    no mail, CI

--- hr_expense_auto_validate/models/hr_expense.py
+++ hr_expense_auto_validate/models/hr_expense.py
@@ -4,11 +4,22 @@ from odoo import _, models
 class HrExpense(models.Model):
     _inherit = "hr.expense"
 
+    def _auto_validate_mail_suppression_context(self):
+        return {
+            "default_notify": False,
+            "mail_auto_subscribe_no_notify": True,
+            "mail_notify_force_send": False,
+            "mail_post_autofollow": False,
+            "tracking_disable": True,
+        }
+
     def action_submit_expenses(self):
         """Create report(s) and trigger auto submit/approve/post flow."""
         context_vals = self._get_default_expense_sheet_values()
         sheets = self.env["hr.expense.sheet"].create(context_vals)
-        sheets.sudo().action_submit_sheet()
+        sheets.sudo().with_context(
+            **self._auto_validate_mail_suppression_context()
+        ).action_submit_sheet()
 
         if len(sheets) > 1:
             return {
--- hr_expense_auto_validate/models/hr_expense_sheet.py
+++ hr_expense_auto_validate/models/hr_expense_sheet.py
@@ -4,10 +4,21 @@ from odoo import models
 class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
 
+    def _auto_validate_mail_suppression_context(self):
+        return {
+            "default_notify": False,
+            "mail_auto_subscribe_no_notify": True,
+            "mail_notify_force_send": False,
+            "mail_post_autofollow": False,
+            "tracking_disable": True,
+        }
+
     def action_submit_sheet(self):
         """Submit, auto-approve and post accounting entries."""
-        result = super().action_submit_sheet()
-        sheets = self.sudo()
+        sheets = self.sudo().with_context(
+            **self._auto_validate_mail_suppression_context()
+        )
+        result = super(HrExpenseSheet, sheets).action_submit_sheet()
         sheets._do_approve()
         approved_sheets = sheets.filtered(lambda sheet: sheet.state == "approve")
         if approved_sheets:
